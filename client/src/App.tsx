000d1dff31f41d4c1032a3f1362614bbb2fb2567
rlaclgh <rlaclgh11@gmail.com>
2024-06-24 11:24:34 +0900

    first commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./router";
 const queryClient = new QueryClient();
 const App = () => {
+  console.log(11);
   return (
     <>
       <QueryClientProvider client={queryClient}>
